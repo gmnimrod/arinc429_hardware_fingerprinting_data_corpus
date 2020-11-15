479ce5633525509902f68d79b6ef05698958f900
gmnimrod <gmnimrod@gmail.com>
2020-11-15 21:53:53 +0200

    Use github styles instead of custom css.

--- scripts/build.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-
-TITLE="ARINC429 Data Corpus - Hardware fingerprinting for the ARINC 429 avionic bus"
-pandoc -s src/README.md -o index.html -H src/github-pandoc.css --metadata title="$TITLE"
-
-# or -c github-pandoc.css if you intend to include css with html. Remove <style></style> then.
